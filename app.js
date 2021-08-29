60b2077bbc6496aa13491ae4c614f6d3dd0b703b
munroe1786 <era3munroe@gmail.com>
2021-08-28 20:40:25 -0500

    add theTetrominoes array to store tetrominoes and add currentPosition and current variables / add draw function / add .tetromino with bg color to css file

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ document.addEventListener('DOMContentLoaded', () => {
     //The Tetrominoes
     const lTetromino = [
         [1, width+1, width*2+1, 2],  
-        [width, width+1, width*2, width*2+2],
+        [width, width*2+1, width*2, width*2+2],
         [1, width+1, width*2+1, width*2],
         [width, width*2, width*2+1, width*2+2]
     ]
 
-    
+    const theTetriminoes = [lTetromino]
+
+    let currentPosition = 4
+    let current = theTetriminoes[0][1]
+
+    function draw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add('tetromino')
+        })
+    }
+
+    draw()
 })
