d7714c8fcaef45c3ba33388ac6ba8258f015e6f2
munroe1786 <era3munroe@gmail.com>
2021-08-30 22:52:28 -0500

    add notes for understanding and add undraw function to remove tetrominoes

--- app.js
+++ app.js
@@ -44,16 +44,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const theTetrominoes = [lTetromino, oTetromino, iTetromino, tTetromino, zTetromino]
 
     let currentPosition = 4
-
+    let currentRotation = 0
+    
+    //randomly select a Tetromino and its first rotation
     let random = Math.floor(Math.random()*theTetrominoes.length)
-    console.log(random)
-    let current = theTetrominoes[0][0]
+    let current = theTetrominoes[random][currentRotation]
 
+    //draw the Tetromino
     function draw() {
         current.forEach(index => {
             squares[currentPosition + index].classList.add('tetromino')
         })
     }
 
-    draw()
+    //undraw the Tetromino
+    function undraw() {
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove('tetromino')
+        })
+    }
 })
