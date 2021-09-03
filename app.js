8ff60914d871d4e6a3ec013de116673f184b2e70
munroe1786 <era3munroe@gmail.com>
2021-09-02 22:05:43 -0500

    add moveLeft function

--- app.js
+++ app.js
@@ -87,4 +87,20 @@ document.addEventListener('DOMContentLoaded', () => {
             //draw()
         }
     }
+
+    //move the tretromino left, unless it's at the edge or is blocked
+    function moveLeft() {
+        undraw()
+        const isAtLeftEdge = current.some(index => (currentPosition + index) % width === 0)
+
+        if(!isAtLeftEdge) currentPosition -=1
+
+        if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
+            currentPosition +=1
+        }
+
+        draw()
+    }
+
+
 })
