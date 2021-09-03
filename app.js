708eef27fa204ef294427b954f0a3fc07e09d25c
munroe1786 <era3munroe@gmail.com>
2021-09-02 21:33:32 -0500

    add freeze function

--- app.js
+++ app.js
@@ -73,4 +73,15 @@ document.addEventListener('DOMContentLoaded', () => {
         currentPosition += width
         //draw()
     }
+
+    function freeze() {
+        if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+            current.forEach(index => squares[currentPosition + index].classList,add('taken'))
+            //start a new tetromino falling
+            random = Math.floor(Math.random() * theTetrominoes.length)
+            current = theTetrominoes[random][currentRotation]
+            currentPosition = 4
+            draw()
+        }
+    }
 })
