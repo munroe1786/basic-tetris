4fa5ae9e69323027adeb303ed8ac9eccc6ffba1d
munroe1786 <era3munroe@gmail.com>
2021-09-07 22:12:32 -0500

    add displayShape function to show the shape in the mini-grid / add next Random to file and to freeze function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const ScoreDisplay = document.querySelector('#score')
     const StartButton = document.querySelector('#start-button')
     const width = 10
+    let nextRandom = 0
 
     //The Tetrominoes
     const lTetromino = [
@@ -96,10 +97,12 @@ document.addEventListener('DOMContentLoaded', () => {
         if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
             current.forEach(index => squares[currentPosition + index].classList.add('taken'))
             //start a new tetromino falling
-            random = Math.floor(Math.random() * theTetrominoes.length)
+            radom = nextRandom
+            nextRandom = Math.floor(Math.random() * theTetrominoes.length)
             current = theTetrominoes[random][currentRotation]
             currentPosition = 4
-            //draw()
+            draw()
+            displayShape()
         }
     }
 
@@ -146,6 +149,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const displaySquares = document.querySelectorAll('.mini-grid div')
     const displayWidth = 4
     let displayIndex = 0
+    let nextRandom = 0
 
     //the Tetrominos without rotations
     const upNextTetrmonioes = [
@@ -156,5 +160,13 @@ document.addEventListener('DOMContentLoaded', () => {
         [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] //iTetromino
     ]
 
+    //display the shape in the mini-grid display
+    function displayShape() {
+        //remove any trace of a tetromino from the entire grid
+        displaySquares.forEach(square => {
+            square.classList.remove('tetromino')
+        })
+        upNextTetrmonioes[nextRandom ]
+    }
 
 })
