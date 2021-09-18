5deba1364e35f9e63c7915ad0904dcc9629b4300
munroe1786 <era3munroe@gmail.com>
2021-09-17 22:31:41 -0500

    start addScore function

--- app.js
+++ app.js
@@ -185,4 +185,10 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    //add score
+    function addScore() {
+        for (let i=0; i < 199; i +=width) {
+            const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+        }
+    }
 })
