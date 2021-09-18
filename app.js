d95d87c5d11c93068d9ae00509f72cd5d9e30c49
munroe1786 <era3munroe@gmail.com>
2021-09-17 22:52:00 -0500

    add remove tetromino to row.forEach in addScore function

--- app.js
+++ app.js
@@ -196,6 +196,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 scoreDisplay.innerHTML = score
                 row.forEach(index => {
                     squares[index].classList.remove('taken')
+                    squares[index].classList.remove('tetromino')
                 })
                 const squaresRemoved = squares.splice(i, width)
                 squares = squaresRemoved.concat(squares)
